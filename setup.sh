ea5c0ad792ddd7ab959c5fdca3f240ef4cab4a36
luis miguel yances castillo <Luisyances1@gmail.com>
2023-04-25 21:29:44 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,3 @@
-mkdir -p ~/.streamlit/
-echo  "
-[theme]
-base='dark'
-primaryColor='#01106e'
-secondaryBackgroundColor='#05389e'
-font='timenewsromans'
-[server]
-headless = true
-enableCORS=false
-enableXsrfProtection=false
-port = $PORT
-"  > ~/.streamlit/config.toml
 cd /frontend && npm i
 cd ..
 npm run build
